b43f5fd67cdb18180b979ce985bd3af9b1c8c381
Nicholas Makhija <nicholasmakhija@gmail.com>
2026-03-06 23:46:36 +1100

    fix: last section spacing

--- src/ui/Section.ts
+++ src/ui/Section.ts
@@ -1,12 +1,12 @@
 import styled from '@styled';
 
-import { contentParadigm } from '@utils';
+import { asRem } from '@utils/as-rem';
 import {
   CHILD_PARADIGM,
   CONTENT_SPACER
 } from '@constants';
 
 export const Section = styled.section({
-  ...contentParadigm(CONTENT_SPACER * 2),
+  margin: `0 0 ${asRem(CONTENT_SPACER * 2)}`,
   ...CHILD_PARADIGM
 });
