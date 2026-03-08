513fb18ab40865f698ab2c93301725a77f27d9c5
Nicholas Makhija <nicholasmakhija@gmail.com>
2026-03-09 09:02:20 +1100

    chore: simplified getPages ditched pipe

--- server/get-pages.ts
+++ server/get-pages.ts
@@ -1,8 +1,6 @@
 import { readFileSync } from 'node:fs';
 import minifier from 'html-minifier';
 
-import { pipe } from './pipe';
-
 const readFile = (file: string) =>
   readFileSync(`src/content/${file}`, 'utf8');
 
@@ -18,39 +16,24 @@ const getHTML = (file: string) => {
   return htmlContent;
 };
 
-type Manifest = {
-  [key: string]: {
-    title: string;
-    sections: {
-      title: string;
-      id: string;
-      content: string;
-    }[];
-  };
-};
-
-const createContentTree = (data: Manifest) => Object
-  .entries(data)
-  .reduce((acc, [path, { title, sections }]) => ({
-    ...acc,
-    [path]: {
-      title,
-      sections: sections.map(({ title, id, content }) => ({
-        title,
-        id,
-        content: getHTML(content)
-      }))
-    }
-  }), {});
-
-export const compose = pipe<string>()
-  .then(readFile)
-  .then((file) => file
+export const getPages = () => {
+  const jsonString = readFile('manifest.html')
     .replace('<script>const manifest = ', '')
-    .replace(';</script>', '')
-  )
-  .then(JSON.parse)
-  .then(createContentTree);
+    .replace(';</script>', '');
 
-export const getPages = () => compose('manifest.html');
+  const manifest = JSON.parse(jsonString) as PageManifest;
 
+  return Object
+    .entries(manifest)
+    .reduce((acc, [path, { title, sections }]) => ({
+      ...acc,
+      [path]: {
+        title,
+        sections: sections.map(({ title, id, content }) => ({
+          title,
+          id,
+          content: getHTML(content)
+        }))
+      }
+    }), {});
+};
