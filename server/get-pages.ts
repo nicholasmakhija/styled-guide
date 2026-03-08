a77193e43670c9a4fcb3fa00967bdc7c71f2cbba
Nicholas Makhija <nicholasmakhija@gmail.com>
2026-03-08 19:46:31 +1100

    refactor: moved manifest to script in html for faster server reloads

--- server/get-pages.ts
+++ server/get-pages.ts
@@ -1,9 +1,13 @@
 import { readFileSync } from 'node:fs';
 import minifier from 'html-minifier';
-import { manifest } from '../src/content/manifest';
+
+import { pipe } from './pipe';
+
+const readFile = (file: string) =>
+  readFileSync(`src/content/${file}`, 'utf8');
 
 const getHTML = (file: string) => {
-  const raw = readFileSync(`src/content/${file}`, 'utf8');
+  const raw = readFile(file);
   /* eslint-disable @typescript-eslint/naming-convention */
   const htmlContent = minifier.minify(raw, {
     collapseWhitespace: true,
@@ -14,8 +18,19 @@ const getHTML = (file: string) => {
   return htmlContent;
 };
 
-export const getPages = () => Object
-  .entries(manifest)
+type Manifest = {
+  [key: string]: {
+    title: string;
+    sections: {
+      title: string;
+      id: string;
+      content: string;
+    }[];
+  };
+};
+
+const createContentTree = (data: Manifest) => Object
+  .entries(data)
   .reduce((acc, [path, { title, sections }]) => ({
     ...acc,
     [path]: {
@@ -27,4 +42,15 @@ export const getPages = () => Object
       }))
     }
   }), {});
+
+export const compose = pipe<string>()
+  .then(readFile)
+  .then((file) => file
+    .replace('<script>const manifest = ', '')
+    .replace(';</script>', '')
+  )
+  .then(JSON.parse)
+  .then(createContentTree);
+
+export const getPages = () => compose('manifest.html');
 
