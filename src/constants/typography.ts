c34ed1fde45c5e868137f912e71f2a8dadbf3097
Nicholas Makhija <nicholasmakhija@gmail.com>
2026-03-10 20:17:58 +1100

    refactor: leading arg is now required in typography util

--- src/constants/typography.ts
+++ src/constants/typography.ts
@@ -4,15 +4,15 @@ import type { CSSProperties } from '@n3e/styled';
 
 const typography = (
   size: number,
-  leading?: number
+  leading: number
 ): CSSProperties => ({
   fontSize: asRem(size),
-  lineHeight: leading ? asRem(leading) : undefined
+  lineHeight: asRem(leading)
 });
 
 export const H1 = typography(32, 40);
 export const H2 = typography(24, 32);
 export const H3 = typography(18, 32);
 export const CODE = typography(14, 20);
-export const SMALL = typography(14);
-export const TINY = typography(12);
+export const SMALL = typography(14, 24);
+export const TINY = typography(12, 24);
